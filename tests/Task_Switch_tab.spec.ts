51abb36dd0d2c40be691469c6e5f8529940cdfba
UserDhanesh <dhanesh.g@xminds.com>
2026-08-09 16:53:02 +0530

    update

--- tests/Task_Switch_tab.spec.ts
+++ tests/Task_Switch_tab.spec.ts
@@ -1,20 +1,25 @@
-import { test } from '@playwright/test';
-test('Switchtab', async ({ page, context }) => {
+import { test, expect } from '@playwright/test';
+test('Dropdownbox', async ({ page,context }) => {
 await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
-console.log('main Site reached');
-await page.getByRole('heading', { name: 'Practice page' }).click();
-console.log('Heading :-Practice page');
-await page.waitForTimeout(2000);
-await page.getByText('Switch Tab Example', { exact: true }).click();
-console.log('Switch Tab Example');
-await page.waitForTimeout(2000);
+await expect(page).toHaveURL('https://rahulshettyacademy.com/AutomationPractice/');
+console.log('main Site reached and verified');
+const heading1 =page.getByRole('heading', { name: 'Practice page' });
+await expect(heading1).toBeVisible();
+console.log('Heading :-Practice page are verified');
+const heading2 =await page.getByText('Switch Tab Example', { exact: true });
+await expect(heading2).toBeVisible();
+console.log('Heading :-Switch tab example');
+const theTab =await page.getByRole('link', { name: 'Open Tab' });
+await expect(theTab).toBeVisible();
+console.log('Open tab button visible ');
 const [newTab] = await Promise.all([
 context.waitForEvent('page'),
-page.getByRole('link', { name: 'Open Tab' }).click(),
-]);
+theTab.click()]);
 console.log('New tab opened');
- await page.waitForTimeout(2000);
-  await newTab.close();
-  console.log('New tab closed');
-  await page.waitForTimeout(2000);
+await newTab.waitForLoadState();
+const courses = newTab.getByText('Access all our Courses', { exact: true });
+await expect(courses).toBeVisible();
+console.log('Access all our Courses is visible in new tab');
+await page.close();
+console.log('close the Tab');
 });
