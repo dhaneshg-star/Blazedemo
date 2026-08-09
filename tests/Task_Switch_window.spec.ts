e02d102d04127d7883d96a6b243211a4a17da22c
UserDhanesh <dhanesh.g@xminds.com>
2026-08-09 15:52:41 +0530

    new updated

--- tests/Task_Switch_window.spec.ts
+++ tests/Task_Switch_window.spec.ts
@@ -1,21 +1,23 @@
-import { test } from '@playwright/test';
+import { test,expect } from '@playwright/test';
 test('Great1', async ({ page, context }) => {
 await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
-console.log('main Site reached');
-await page.getByRole('heading', { name: 'Practice page' }).click();
-console.log('Heading :-Practice page');
-await page.waitForTimeout(2000);
-await page.getByText('Switch Window Example', { exact: true }).click();
-console.log('Switch window Example');
-await page.waitForTimeout(2000);
+await expect(page).toHaveURL('https://rahulshettyacademy.com/AutomationPractice/');
+console.log('main Site reached and verified');
+const heading1 =page.getByRole('heading', { name: 'Practice page' });
+await expect(heading1).toBeVisible();
+console.log('Heading :-Practice page are verified');
+const heading2 =await page.getByText('Switch Window Example', { exact: true });
+await expect(heading2).toBeVisible();
+console.log('Heading :-Switch window example page verified ');
 const [newPage] = await Promise.all([
 context.waitForEvent('page'),
 page.getByRole('button', { name: 'Open Window' }).click(),
   ]);
 console.log('New window opened');
-await page.waitForTimeout(3000);
-
+await newPage.waitForLoadState();
+const accessCourses = newPage.getByText('Access all our Courses', {exact: true});
+await expect(accessCourses).toBeVisible();
+console.log('Heading new tab :-Access all our Courses is visible and verified');
 await newPage.close();
 console.log('New window closed');
-await page.waitForTimeout(2000);
 });
--- tests/Task_checkbox.spec.ts
+++ tests/Task_checkbox.spec.ts
@@ -1,27 +1,32 @@
 import { test, expect } from '@playwright/test';
-
 test('Checkbox Validation', async ({ page }) => {
-  await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
-  console.log('main Site reached');
-  await page.getByRole('heading', { name: 'Practice page' }).click();
-  console.log('Heading :- Practice page');
-  await page.getByText('Checkbox Example', { exact: true }).click();
-  console.log('Checkbox Example');
+await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+await expect(page).toHaveURL('https://rahulshettyacademy.com/AutomationPractice/');
+console.log('main Site reached and verified');
+const heading1 =page.getByRole('heading', { name: 'Practice page' });
+await expect(heading1).toBeVisible();
+console.log('Heading :-Practice page are verified');
+const heading2 =await page.getByText('Checkbox Example', { exact: true });
+await expect(heading2).toBeVisible();
+console.log('Heading :-checkbox example page verified ');
   // Select Option 1
-  const checkbox1 = page.locator('#checkBoxOption1');
-  await checkbox1.check();
-  await expect(checkbox1).toBeChecked();
-  console.log('Option1 enabled and verified');
+const checkbox1 = page.locator('#checkBoxOption1');
+await checkbox1.check();
+const option1Text = await page.locator('label:has-text("Option1")');
+await expect(option1Text).toBeVisible();
+console.log('Option1 "box" can be select and "Text" able to view are verified');
   // Select Option 2
-  const checkbox2 = page.locator('#checkBoxOption2');
-  await checkbox2.check();
-  await expect(checkbox2).toBeChecked();
-  console.log('Option2 enabled and verified');
+const checkbox2 = page.locator('#checkBoxOption2');
+await checkbox2.check();
+const option2Text = await page.locator('label:has-text("Option2")');
+await expect(option2Text).toBeVisible();
+console.log('Option2 "box" can be select and "Text" able to view are verified');
   // Select Option 3
-  const checkbox3 = page.locator('#checkBoxOption3');
-  await checkbox3.check();
-  await expect(checkbox3).toBeChecked();
-  console.log('Option3 enabled and verified');
+const checkbox3 = page.locator('#checkBoxOption3');
+await checkbox3.check();
+const option3Text = await page.locator('label:has-text("Option3")');
+await expect(option3Text).toBeVisible();
+console.log('Option3 "box" can be select and "Text" able to view are verified');
   // Verify multiple checkboxes can remain selected
   await expect(checkbox1).toBeChecked();
   await expect(checkbox2).toBeChecked();
